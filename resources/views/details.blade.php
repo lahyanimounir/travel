f8f1a72be6934eb2ffaa75e615f356d19dcefbf3
lahyani <lahyani.mo@gmail.com>
2020-03-09 18:33:39 +0100

    09/03/2020

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -50,5 +50,123 @@
         </div>
       </div>
     </section>
+
+     <section >
+      <div class="container" style="padding-left: 40px ">
+        <div class="row">
+          <div class="col-12">
+            <p style="font-size: 14px;text-align: justify;">Traversez le plus haut col d'Afrique du Nord, la route des montagnes de l'Atlas et Tizi N Tichka (2260 m) en profitant du magnifique paysage le long de la route de la célèbre Kasbah d'Ait Ben Haddou, site du patrimoine mondial depuis 1987. Cette visite privée du désert de Marrakech aux dunes de Zagora à travers la porte du désert d'Ourzazate, la vallée de la Deraa et la ville d'Agdez est le meilleur moyen de découvrir le Maroc authentique, le mode de vie des nomades berbères, la culture locale lors d'une soirée animée avec de la musique nomade. du Sahara et passez la nuit sous les étoiles dans une tente saharienne privée dans le désert, après une balade à dos de chameau dans le désert aride du Sahara de Zagora.</p>
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="border-bottom: 1px solid #ddd;height:50px;font-size: 13px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            DESTINATION
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="border-bottom: 1px solid #ddd;height:50px;font-size: 13px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            LIEU DE SORTIE
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="border-bottom: 1px solid #ddd;height:50px;font-size: 13px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            LIEU DE RETOUR
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="border-bottom: 1px solid #ddd;height:50px;font-size: 13px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            HEURE DE SORTIE 
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="border-bottom: 1px solid #ddd;height:50px;font-size: 13px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            HEURE DE RETOUR
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="border-bottom: 1px solid #ddd;height:50px;font-size: 13px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            DURÉE
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+
+        <div class="row align-items-center" style="font-size: 13px;;border-bottom: 1px solid #ddd;height:50px; ">
+          <div class="col-md-3" style="font-weight: bold;">
+            PRIX
+          </div>
+          <div class="col-md-9">
+            Vers le désert de Zagora et les kasbahs
+          </div>
+        </div>
+          <br>
+        <div class="row">
+          <div class="col-12">
+             <p style="font-size: 14px;text-align: justify;font-weight: bold;">Jour 1 : Agadir - Mirleft - Sidi Ifni - Fort Bou Jerif. 230 km</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <p style="font-size: 14px;text-align: justify;">Départ plein Sud en longeant la côte ou les pécheurs vivent dans des abris troglodytes creusés des les falaises. Nous traverserons la réserve naturelle de Souss Massa que nous contournerons. Après avoir traversé le village de Massa et de Tassilla nous repiquerons vers la mer pour rejoindre Mirleft, puis Sidi Ifni là nous longerons la pointe de l'Atlas. Arrivée à Fort Bou Jerif en fin d’après-midi. Dîner et nuit en tente nomade.</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+             <p style="font-size: 14px;text-align: justify;font-weight: bold;">Jour 2 : Agadir - Mirleft - Sidi Ifni - Fort Bou Jerif. 230 km</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-12">
+            <p style="font-size: 14px;text-align: justify;">
+               Le matin nous quittons la Forte direction Guelmim porte du désert marocain, pour continuer vers Amtoudi et sa source, endroit sublime, vous emprunterez de nouvelles pistes à travers les contreforts de l’Anti-Atlas où les oasis et les petits villages berbères sont entourés d’un paysage de montagnes désertiques. Vous atteindrez la plénitude en arrivant le soir à Tafraoute, bourgade située à 1200 m d'altitude, au milieu d'un cirque d'énormes blocs de granit rose qui s'embrasent au soleil couchant dans de magnifiques nuances de rose, d'ocre et de mauve. Visite du village. Dîner et nuit à l'hôtel.
+            </p>
+          </div>
+        </div>
+
+        
+      
+        <br>
+        <div class="row">
+          <div class="col-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m58!1m12!1m3!1d1040828.9465384332!2d-7.814769993371298!3d30.9876688155964!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m43!3e0!4m5!1s0xdafee8d96179e51%3A0x5950b6534f87adb8!2sMarrakesh!3m2!1d31.6294723!2d-7.9810845!4m5!1s0xdbae06120411439%3A0x4d090f64a0ec123a!2sA%C3%AFt+Ben+Haddou!3m2!1d31.047043!2d-7.1318996!4m5!1s0xdbb80538b38a023%3A0x5668a92d056600dd!2sAgdz!3m2!1d30.7041312!2d-6.4530072999999994!4m5!1s0xdbc3438bd3d2639%3A0xa96949621562564e!2sZagora+Desert+Camp%2C+Zagora!3m2!1d30.371474!2d-5.76751!4m5!1s0xdbb81d9ed83a5bd%3A0xccfe6c950f340402!2sDraa+river+panoramic%2C+Agdz!3m2!1d30.692558499999997!2d-6.442831!4m5!1s0xdbb104077422057%3A0x26b3cb529b37ab00!2sOuarzazate!3m2!1d30.9335436!2d-6.937016!4m5!1s0xdafee8d96179e51%3A0x5950b6534f87adb8!2sMarrakech!3m2!1d31.6294723!2d-7.9810845!5e0!3m2!1sen!2sma!4v1563651389818!5m2!1sen!2sma" width="100%" height="550" frameborder="0" style="border:0" allowfullscreen=""></iframe>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="col-12 text-center">
+              <a href="#" style="background: #efa700;color: #fff;border-radius: 5px;padding: 7px 9px;cursor: pointer;">Réserver Maintenant</a>
+          </div>
+        </div>
+
+        
+      </div>
+    </section>
+<br>
+<br>
+
 </main>
 @endsection
